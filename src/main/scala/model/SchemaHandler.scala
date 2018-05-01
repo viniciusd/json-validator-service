ccc8952c2a20bbe649c73b3a58fdb68dbe5b5252
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2018-05-01 16:40:40 -0400

    Remove license header from the SchemaHandler

--- src/main/scala/model/SchemaHandler.scala
+++ src/main/scala/model/SchemaHandler.scala
@@ -1,18 +1,3 @@
-// See the LICENCE.txt file distributed with this work for additional
-// information regarding copyright ownership.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package model
 
 import akka.actor.{Actor, ActorLogging}
@@ -32,6 +17,9 @@ class SchemaHandler() extends Actor with ActorLogging {
   import SchemaHandler._
   implicit val ec = context.dispatcher
   override def receive: Receive = {
+    // closing over the sender in Future is not safe
+    // I am keeping those references just in case for now
+
     case Validate(id) =>
       val _sender = sender()
       _sender ! SchemaValidated("validateDocument", id, "success")
@@ -41,8 +29,6 @@ class SchemaHandler() extends Actor with ActorLogging {
       _sender ! SchemaUploaded("uploadSchema", id, "success")
 
     case Get(id) =>
-      //closing over the sender in Future is not safe
-      //http://helenaedelson.com/?p=879
       val _sender = sender()
       _sender ! Schema(id)
   }
